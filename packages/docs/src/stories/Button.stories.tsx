d08a24e5beed4c3f3741b0d1347283f8e88375ce
Diego Goulart <diegogoulart.aws@gmail.com>
2023-02-26 19:44:16 -0300

    Refactoring Button component

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,24 +1,54 @@
 import { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@atlantis-ui/react'
+import { ArrowRight } from 'phosphor-react'
 
 export default {
   title: 'Form/Button',
   component: Button,
 
   args: {
-    children: 'Click me!',
-    size: 'small',
+    children: 'Send',
+    disabled: false,
   },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
-export const Small: StoryObj<ButtonProps> = {
+export const Secondary: StoryObj<ButtonProps> = {
   args: {
-    size: 'small',
+    variant: 'secondary',
+    children: 'Create',
   },
 }
-export const Big: StoryObj<ButtonProps> = {
+export const Tertiary: StoryObj<ButtonProps> = {
   args: {
-    size: 'big',
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next step
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+export const Medium: StoryObj<ButtonProps> = {
+  args: {
+    size: 'md',
+  },
+}
+export const Large: StoryObj<ButtonProps> = {
+  args: {
+    size: 'lg',
   },
 }
--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -1,29 +1,89 @@
-import { ComponentProps } from 'react'
+import { ComponentProps } from '../types/component-props'
 import { styled } from './../styles'
 
 export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$green500',
+  all: 'unset',
   borderRadius: '$sm',
-  border: 0,
-  fontWeight: 'bold',
-  color: '$white',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 120,
+  boxSizing: 'border-box',
+  padding: '0 $4',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
 
   variants: {
-    size: {
-      small: {
-        fontSize: '$sm',
-        padding: '$2 $4',
+    variant: {
+      primary: {
+        color: '$white',
+        backgroundColor: '$green500',
+
+        '&:not(:disabled):hover': {
+          backgroundColor: '$green300',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
       },
-      big: {
-        fontSize: '$md',
-        padding: '$3 $6',
+      secondary: {
+        color: '$green300',
+        border: '2px solid $green500',
+
+        '&:not(:disabled):hover': {
+          backgroundColor: '$green500',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+    size: {
+      sm: {
+        height: 38,
+      },
+      md: {
+        height: 44,
+      },
+      lg: {
+        height: 52,
       },
     },
   },
 
   defaultVariants: {
-    size: 'small',
+    variant: 'primary',
+    size: 'md',
   },
 })
 
